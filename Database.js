1731670d4589c4eb78c3351733266885147f8166
José Yoshiriro <jyoshiriro@gmail.com>
2018-10-29 13:11:34 -0200

    Melhorando o tratamento da conexão aberta

--- Database.js
+++ Database.js
@@ -6,12 +6,14 @@ var config = require("./config").database;
 
 var query = function(queryString = '') {
 	var sql = require('mssql');
+	sql.close();
 	return new Promise((res, rej) => {
         sql.connect(config).then(pool => {
             return pool.request().query(queryString);
         }).then(results => {
             res(results);
         }).catch(e => {
+			sql.close();
             rej(e);
         }).finally(() => {
             sql.close();
